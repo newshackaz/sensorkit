f992e2d1b7e0c593e86354c5a0e235f38c5e75f4
Scott Pham <scott.pham85@gmail.com>
2017-02-09 09:26:38 -0800

    fixed erros in temp.py

--- putting_it_together/temp_humidity/temp.py
+++ putting_it_together/temp_humidity/temp.py
@@ -3,7 +3,7 @@ from time import sleep
 # datetime gets us dates, timedelta for doing math with dates
 from datetime import datetime, timedelta
 # the main rpi library
-import Rpi.GPIO as GPIO
+import RPi.GPIO as GPIO
 # load the sensor library
 import Adafruit_DHT as DHT
 
@@ -11,13 +11,13 @@ import Adafruit_DHT as DHT
 # set up Rpi numbering system
 GPIO.setmode(GPIO.BCM)
 # set up sensor
-SENSOR = Adafruit_DHT.DHT22
+SENSOR = DHT.DHT22
 # this is where we read the sensor data
 INPUT_PIN = 4
 
 
 # set up a temp conversion function
-def convertTemp(celcius):
+def convert_temp(celcius):
     return celcius * float(9) / float(5) + 32
 
 # set up a reading function
@@ -25,11 +25,13 @@ def get_reading():
     # get datestring    
     now = datetime.now().strftime("%Y-%m-%d %I:%M %p")
     # get hum and temp from sensor
-    humidity, temperature = DHT.read_retry(SENSOR, PIN)
+    humidity, temperature = DHT.read_retry(SENSOR, INPUT_PIN)
     # convert temperature
     temperature = convert_temp(temperature)
-    print "temperature = " + temperature
-    print "humidity = " + humidity
+    print "temperature = "
+    print temperature
+    print "humidity = "
+    print humidity
 
 # get a reading every 10 seconds
 while True:
