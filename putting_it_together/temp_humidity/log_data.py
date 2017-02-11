1b5a3b7f0ec2d3c3f457934e2f5ab07d4945c9fe
Scott Pham <scott.pham85@gmail.com>
2017-02-11 14:39:14 -0800

    script writes headers correctly

--- putting_it_together/temp_humidity/log_data.py
+++ putting_it_together/temp_humidity/log_data.py
@@ -16,9 +16,25 @@ SENSOR = DHT.DHT22
 # this is where we read the sensor data
 INPUT_PIN = 4
 
+# store the csv name because we'll use it later
+DATACSV = 'backup.csv'
+
 # set up a temp conversion function
 def convert_temp(celcius):
     return celcius * float(9) / float(5) + 32
+
+# check csv for a header:
+def check_header():
+    # open csv for reading
+    try:
+        mycsv = open(DATACSV, 'rb')
+    except:
+        # open csv for writing:
+	mycsv = open(DATACSV, 'wb')
+	# make the csv writer object
+	writer = csv.writer(mycsv)
+	# write the header row
+	writer.writerow(['date', 'temperature', 'humidity'])
 
 # set up a reading function
 def get_reading():
@@ -31,11 +47,12 @@ def get_reading():
     print "temperature = {0}".format(temperature)
     print "humidity = {0}".format(humidity)
     
-    myCsv = open('data.csv', 'a')
+    myCsv = open(DATACSV, 'a')
     csvWriter = csv.writer(myCsv)
     csvWriter.writerow([now, round(temperature, 2), round(humidity, 2)])
     myCsv.close()
 
+check_header()
 # get a reading every 10 seconds
 while True:
     get_reading()
